c24c8985fdab237e07add089be020393073dd8c7
slbedwell <humanno626@gmail.com>
2023-03-05 19:13:53 +0000

    Add loading component

---
+++ components/LoadingOverlay.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-overlay
+        v-model="value"
+        :absolute="!fullPage"
+        color="white"
+        opacity="0.75"
+    >
+        <v-progress-circular
+            indeterminate
+            color="secondary"
+            size="50"
+        />
+    </v-overlay>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Boolean,
+            default: false,
+        },
+        fullPage: {
+            type: Boolean,
+            default: false,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
