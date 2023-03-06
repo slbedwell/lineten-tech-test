28a066e67abd64888942d5633a143916158cd30f
Samantha <humanno626@gmail.com>
2023-03-06 14:00:08 +0000

    Add the ability to go back and forth between months

--- components/RotaCalendar.vue
+++ components/RotaCalendar.vue
@@ -1,12 +1,53 @@
 <template>
     <div>
+        <v-toolbar
+            v-if="$refs.rotaCalendar"
+            flat
+            rounded
+            class="d-flex justify-space-betwee "
+        >
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        v-bind="attrs"
+                        v-on="on"
+                        depressed
+                        @click="previousMonth"
+                    >
+                        <v-icon left>mdi-chevron-left</v-icon>
+                        Previous
+                    </v-btn>
+                </template>
+                View Previous Month
+            </v-tooltip>
+
+            <v-toolbar-title class="mx-4">
+                {{ $refs.rotaCalendar.title }}
+            </v-toolbar-title>
+
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        v-bind="attrs"
+                        v-on="on"
+                        depressed
+                        @click="nextMonth"
+                    >
+                        <v-icon left>mdi-chevron-right</v-icon>
+                        Next
+                    </v-btn>
+                </template>
+                View Next Month
+            </v-tooltip>
+        </v-toolbar>
+
         <v-calendar
-            ref="calendar"
+            ref="rotaCalendar"
+            v-model="value"
             type="month"
             event-name="user"
             event-start="startDate"
             event-end="endDate"
-            :weekdays="[1, 2, 3, 4, 5, 6, 0]"
             :events="rotas"
             :event-color="getEventColor"
         />
@@ -16,18 +57,18 @@
 <script>
 export default {
     props: {
-        value: {
-            type: Array,
-            default() {
-                return [];
-            },
-        },
         rotas: {
             type: Array,
             default() {
                 return [];
             },
         },
+    },
+
+    data() {
+        return {
+            value: undefined
+        }
     },
 
     methods: {
@@ -39,6 +80,12 @@ export default {
                     return 'secondary';
             }
             return 'grey';
+        },
+        nextMonth() {
+            this.$refs.rotaCalendar.next();
+        },
+        previousMonth() {
+            this.$refs.rotaCalendar.prev();
         },
     },
 }
